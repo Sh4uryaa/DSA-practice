9edda28194530e333d44bc0ca4b333b611c17def
Shaurya <shauryandyash@gmail.com>
2026-06-09 17:06:10 +0530

    add pattern-15.cpp

---
+++ Basics/patterns/pattern-15.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (char ch = 'A'; ch <= 'A' + (i-1) ; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
