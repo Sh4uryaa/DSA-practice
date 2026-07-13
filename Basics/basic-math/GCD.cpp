b1ddbcd27f88acc01ca3e4cd72f5af1cec71912d
Sh4uryaa <shaurya.mishra_@outlook.com>
2026-07-13 13:51:38 +0530

    solve GCD problem

---
+++ Basics/basic-math/GCD.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    
+    int a = num1, b = num2;
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    
+    cout << "The GCD of " << num1 << " and " << num2 << " is " << a << ".";
+}
